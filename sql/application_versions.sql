f6a2c59c37f202c5e159cc09b23a5c1ab08c608f
Alex Siryi <desertkun@gmail.com>
2017-01-21 15:03:58 +0200

    Fixed sql issue

--- sql/application_versions.sql
+++ sql/application_versions.sql
@@ -3,8 +3,8 @@ CREATE TABLE `application_versions` (
   `application_version` varchar(255) NOT NULL DEFAULT '',
   `gamespace_id` int(11) unsigned NOT NULL,
   `current_data_version` int(11) unsigned DEFAULT NULL,
-  UNIQUE KEY `application_name` (`application_name`,`gamespace_id`),
+  UNIQUE KEY `application_name` (`application_name`,`gamespace_id`,`application_version`),
   KEY `application_idx` (`application_name`),
   KEY `current_data_version` (`current_data_version`),
   CONSTRAINT `application_versions_ibfk_1` FOREIGN KEY (`current_data_version`) REFERENCES `data_versions` (`version_id`)
-) ENGINE=InnoDB CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
