1bf98aa14dfd380afa0917f16511ec50edeb2882
Alex Siryi <desertkun@gmail.com>
2018-10-03 05:10:36 +0300

    Fixed package data

--- setup.py
+++ setup.py
@@ -7,6 +7,9 @@ DEPENDENCIES = [
 
 setup(
     name='anthill-dlc',
+    package_data={
+      "anthill.dlc": ["anthill/dlc/sql", "anthill/dlc/static"]
+    },
     setup_requires=["pypigit-version"],
     git_version="0.1.0",
     description='Downloadable content (DLC) management service for Anthill platform',
